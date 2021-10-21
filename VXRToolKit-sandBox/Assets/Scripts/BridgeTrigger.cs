45200bf8db970b038186d7f64e524122c2510516
AHDev95 <90920393+AHDev95@users.noreply.github.com>
2021-10-21 15:14:56 -0600

    comments! have been made

--- VXRToolKit-sandBox/Assets/Scripts/BridgeTrigger.cs
+++ VXRToolKit-sandBox/Assets/Scripts/BridgeTrigger.cs
@@ -4,14 +4,14 @@ using UnityEngine;
 
 public class BridgeTrigger : MonoBehaviour
 {
-    [SerializeField] Rigidbody bridge;
+    [SerializeField] Rigidbody bridge; //drop the bridge in here so we can interact  
+                                        //with the rigidbodys Kinematic settings
 
-
-    private void OnTriggerEnter(Collider other)
+    private void OnTriggerEnter(Collider other) //when another collider passes through
     {
-        if (other.gameObject.CompareTag("KeyStone"))
+        if (other.gameObject.CompareTag("KeyStone"))//check for keystone tag
         { 
-            bridge.isKinematic = false;
+            bridge.isKinematic = false;//change the kinematic state to off
 
         }
 
