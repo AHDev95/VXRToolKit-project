cff780146de3404093a5ece33169bd94ed20c75f
AHDev95 <90920393+AHDev95@users.noreply.github.com>
2021-10-21 17:49:06 -0600

    gun made need to work on hook fire

---
+++ VXRToolKit-sandBox/Assets/Scripts/ShootHook.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class ShootHook : MonoBehaviour
+{
+
+    [SerializeField]
+    GameObject hook;
+    [SerializeField]
+    Transform lockpoint;
+
+
+
+
+
+
+
+}
