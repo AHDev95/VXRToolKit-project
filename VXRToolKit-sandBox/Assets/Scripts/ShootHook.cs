dbc685a4870ba15f8c78bcbf2c3f8dfc3c265b03
AHDev95 <90920393+AHDev95@users.noreply.github.com>
2021-10-21 22:18:50 -0600

    finished with doc

--- VXRToolKit-sandBox/Assets/Scripts/ShootHook.cs
+++ VXRToolKit-sandBox/Assets/Scripts/ShootHook.cs
@@ -1,20 +1,53 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.XR.Interaction.Toolkit;
 
 public class ShootHook : MonoBehaviour
 {
-
+    private XRGrabInteractable grabInter;
     [SerializeField]
     GameObject hook;
     [SerializeField]
-    Transform lockpoint;
+    GameObject lockpoint;
 
+    public float hookTravelSpeed; //set the speed of the hook object
+    public float playerTravelSpeed;
 
+   // public static bool fired;
+   //public static bool hooked;
 
+    public float maxDistance;//how far the hook can travel
+    private float currentDistence;//where the hook is currently at
 
+   
 
+    private void Awake()
+    {
+        grabInter = GetComponent<XRGrabInteractable>();
+        grabInter.activated.AddListener(HookShot);
+      
+    }
 
+    private void HookShot(ActivateEventArgs arg0)
+    {
+        hook.transform.Translate(Vector3.back * Time.deltaTime * hookTravelSpeed);
+        currentDistence = Vector3.Distance(transform.position, hook.transform.position);
 
+        if (currentDistence >= maxDistance)
+        {
+            RertunHook();
+        }
+           
+        
+       // Debug.Log("hook fired");
+
+    }
+
+    private void RertunHook()
+    {
+        hook.transform.position = lockpoint.transform.position;
+    }
 }
