020bd958562374cd7362352aa5ade826e223dcc6
AHDev95 <90920393+AHDev95@users.noreply.github.com>
2021-11-04 10:20:05 -0600

    Unity events lab

--- VXRToolKit-sandBox/Assets/Scripts/BubbleShoot.cs
+++ VXRToolKit-sandBox/Assets/Scripts/BubbleShoot.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
 using TMPro;
-
+using UnityEngine.InputSystem;
 
 public class BubbleShoot : MonoBehaviour
 {
@@ -16,11 +16,41 @@ public class BubbleShoot : MonoBehaviour
     private int BubbleAmmo = 10;
     [SerializeField]
     private TextMeshProUGUI ammoText;
+
+    [SerializeField]//realod action
+    InputActionReference reloadAction;
+    XRBaseInteractor interactor;
+    ActionBasedController actionCon;
+
     // Start is called before the first frame update
     void Awake()
     {
         grabInter = GetComponent<XRGrabInteractable>();
         grabInter.activated.AddListener(SpawnBubbles);
+        reloadAction.action.started += Reload;
+    }
+    public void GetInteractor()
+    {
+        interactor = grabInter.selectingInteractor;
+    }
+
+    public void ReleaseInteractor()
+    {
+        interactor = null;
+    }
+
+    private void Reload(InputAction.CallbackContext obj)
+    {
+        if (obj.control.ToString().Contains("Left") && interactor.name.Contains("Left"))
+        {
+            BubbleAmmo = 10;
+            ammoText.text = BubbleAmmo.ToString();
+        }
+        else if (obj.control.ToString().Contains("Right") && interactor.name.Contains("Right"))
+        {
+            BubbleAmmo = 10;
+            ammoText.text = BubbleAmmo.ToString();
+        }
     }
 
     private void SpawnBubbles(ActivateEventArgs arg0)
